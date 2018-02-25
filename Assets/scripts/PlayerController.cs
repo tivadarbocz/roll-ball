769a27ea6f6df1634e9e23dddf341e816fe0d596
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-02-25 19:28:08 +0100

    tutorial part 02

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //before physics calculation
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        
+    }
+}
+ 
